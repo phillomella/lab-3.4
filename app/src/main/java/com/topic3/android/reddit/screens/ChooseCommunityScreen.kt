b365f60e775805b25c13b2a0d9c03288ab8a6a93
phillomella <121259223+phillomella@users.noreply.github.com>
2024-03-08 16:17:00 +0300

    upd ChooseCommunityScreen

--- app/src/main/java/com/topic3/android/reddit/screens/ChooseCommunityScreen.kt
+++ app/src/main/java/com/topic3/android/reddit/screens/ChooseCommunityScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.topic3.android.reddit.R
+import com.topic3.android.reddit.routing.BackButtonAction
 import com.topic3.android.reddit.routing.RedditRouter
 import com.topic3.android.reddit.viewmodel.MainViewModel
 import kotlinx.coroutines.Job
@@ -77,7 +78,9 @@ fun ChooseCommunityScreen(viewModel: MainViewModel, modifier: Modifier = Modifie
         )
         SearchedCommunities(communities, viewModel, modifier)
     }
-
+BackButtonAction {
+    RedditRouter.goBack()
+}
 }
 
 
